8e4fe6091b1947829f5cb6a6c275fb5bd5fb4101
Crownbackend <crownbackend@gmail.com>
2022-11-09 18:50:50 +0100

    add file and login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/{_locale}/', name: 'home')]
     public function home(): Response
     {
         return $this->render('home.html.twig');
+    }
+
+    #[Route('/', name: 'redirect_locale')]
+    public function redirectLocale(Request $request): RedirectResponse
+    {
+        $locale = $request->getLocale();
+        return $this->redirectToRoute('home', [
+            '_locale' => $locale
+        ]);
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,13 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+#[Route(path: '/{_locale}')]
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
          if ($this->getUser()) {
-             return $this->redirectToRoute('target_path');
+             return $this->redirectToRoute('home');
          }
 
         // get the login error if there is one
